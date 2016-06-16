39c60435a2f875816c99c8ff82ff9afea2cc05b1
Publicover <jameslpublicover@gmail.com>
2016-06-15 22:08:57 -0400

    generate beers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :beers
+
+  root 'dashboards#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
