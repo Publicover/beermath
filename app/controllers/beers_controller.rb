8cbc60fb6c1ecd62bb28cd8b27f5fac81436a9a1
Publicover <jameslpublicover@gmail.com>
2016-06-15 22:40:13 -0400

    can save Beer to db with name and brewer

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,10 +1,21 @@
 class BeersController < ApplicationController
 
+  def show
+    @beer = Beer.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:beer].inspect
+    @beer = Beer.new(beer_params)
+
+    @beer.save
+    redirect_to @beer
   end
 
+  private
+    def beer_params
+      params.require(:beer).permit(:name, :brewer)
+    end
 end
