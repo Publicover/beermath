7513145b5d5bb099905761053df724f115fdf77c
Publicover <jameslpublicover@gmail.com>
2016-06-17 22:44:30 -0400

    duff something in the reviews controller, revert to earlier commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616024547) do
+ActiveRecord::Schema.define(version: 20160617215129) do
 
   create_table "beers", force: :cascade do |t|
     t.string   "name"
@@ -23,5 +23,6 @@ ActiveRecord::Schema.define(version: 20160616024547) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 
 end
