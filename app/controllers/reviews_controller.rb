17702be104e2b1ae361e13b3920d1d826c3daf68
Publicover <jameslpublicover@gmail.com>
2016-06-20 14:19:50 -0400

    fix beers/show to display reviews and reviewer name

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,14 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @beer = Beer.find(params[:beer_id])
+    @review = @beer.reviews.create(review_params)
+    redirect_to beer_path(@beer)
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:commenter, :body)
+    end
+
 end
